e72d9c04e978fb8f6a8638a20d800f55a3482fef
Andres Sainz <andresmsainz@gmail.com>
2021-05-06 23:11:59 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Method_Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DisplayName();
+
+        }
+
+        static void DisplayName()
+        {
+            Console.WriteLine("May I have your name?");
+            string sName = Console.ReadLine();
+            WelcomeMessage(sName);
+        }
+
+        static void WelcomeMessage(string strName)
+        {
+            Console.WriteLine("Hello... Jon Snow... eehhh I mean " + strName + "! \nWelcome to my world!");
+        }
+    }
+}
